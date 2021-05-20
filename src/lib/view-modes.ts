a00f81942be3893f74e514da8fa3ed3ab514790d
Yura Frozen <yurafrozen@gmail.com>
2021-05-20 16:04:35 +0500

    last action view mode colors

--- src/lib/view-modes.ts
+++ src/lib/view-modes.ts
@@ -5,63 +5,70 @@ import { GENES } from "./genome";
 import { Key } from "react";
 
 export type VisualiserParams = {
-    ageDivider: number;
-    energyDivider: number,
-    action: { [name: string]: boolean }
+  ageDivider: number;
+  energyDivider: number;
+  action: { [name: string]: boolean };
 };
 
-export type BlockVisualiser = (block: WorldBlock, params: VisualiserParams) => Rgba | null;
+export type BlockVisualiser = (
+  block: WorldBlock,
+  params: VisualiserParams
+) => Rgba | null;
 
 export type ViewMode = {
-    name: string,
-    blockToColor: BlockVisualiser
+  name: string;
+  blockToColor: BlockVisualiser;
 };
 
 const VIEW_MODES: { [key: string]: ViewMode } = {
-    normal: {
-        name: 'Стандартный',
-        blockToColor: (block, params) => block.color,
+  normal: {
+    name: "Стандартный",
+    blockToColor: (block, params) => block.color,
+  },
+  family: {
+    name: "Семейства",
+    blockToColor: (block, params) => block.getFamilyColor(),
+  },
+  age: {
+    name: "Возраст",
+    blockToColor: (block, params) => {
+      return block.getAgeColor(params);
     },
-    family: {
-        name: 'Семейства',
-        blockToColor: (block, params) => block.getFamilyColor(),
+  },
+  energy: {
+    name: "Энергия",
+    blockToColor: (block, params) => {
+      return block.getEnergyColor(params);
     },
-    age: {
-        name: 'Возраст',
-        blockToColor: (block, params) => {
-            return block.getAgeColor(params);
-        },
+  },
+  lastAction: {
+    name: "Последнее действие",
+    blockToColor: (block, params) => {
+      return (
+        block
+          .getLastActionColor(params)
+          ?.interpolate(new Rgba(127, 127, 127, 255), 0.25) || null
+      );
     },
-    energy: {
-        name: 'Энергия',
-        blockToColor: (block, params) => {
-            return block.getEnergyColor(params);
-        },
+  },
+  childrenAmount: {
+    name: "Количество потомков",
+    blockToColor: (block, params) => {
+      return block.getChildrenAmountColor(params);
     },
-    lastAction: {
-        name: 'Последнее действие',
-        blockToColor: (block, params) => {
-            return block.getLastActionColor(params);
-        },
+  },
+  ability: {
+    name: "Специализация в питании",
+    blockToColor: (block, params) => {
+      return block.getAbilityColor();
     },
-    childrenAmount: {
-        name: 'Количество потомков',
-        blockToColor: (block, params) => {
-            return block.getChildrenAmountColor(params);
-        },
+  },
+  health: {
+    name: "Здоровье",
+    blockToColor: (block, params) => {
+      return block.getHealthColor();
     },
-    ability: {
-        name: 'Специализация в питании',
-        blockToColor: (block, params) => {
-            return block.getAbilityColor();
-        },
-    },
-    health: {
-        name: 'Здоровье',
-        blockToColor: (block, params) => {
-            return block.getHealthColor();
-        },
-    },
+  },
 };
 
 export default VIEW_MODES;
