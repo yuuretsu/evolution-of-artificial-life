d0e3df89b4c0246eb8e1343a8f82c25585ab85eb
Yura Morozov <yurafrozen@gmail.com>
2023-02-11 21:22:00 +0500

    Checkbox: ident size

--- src/App/Sidebar/Checkbox.tsx
+++ src/App/Sidebar/Checkbox.tsx
@@ -1,75 +1,75 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.label`
-    display: block;
-    user-select: none;
-    cursor: pointer;
-    &:not(:last-child) {
-        margin-bottom: 5px;
-    }
+  display: block;
+  user-select: none;
+  cursor: pointer;
+  &:not(:last-child) {
+    margin-bottom: 5px;
+  }
 `;
 
 const Input = styled.input.attrs({
-    type: "checkbox",
+  type: "checkbox",
 })`
-    position: relative;
-    appearance: none;
-    margin: 0;
-    margin-right: 5px;
-    outline: none;
-    border: none;
-    &:checked::after {
-        transform: rotateZ(-45deg);
-        opacity: 1;
-    }
-    &::before {
-        content: "";
-        box-sizing: border-box;
-        transform: translate(0px, 2px);
-        display: block;
-        width: 15px;
-        height: 15px;
-        border-radius: 2px;
-        background-color: rgb(80, 80, 80);
-        transition-duration: 0.2s;
-    }
-    &::after {
-        content: "";
-        position: absolute;
-        display: block;
-        left: 1px;
-        top: 3px;
-        width: 10px;
-        height: 5px;
-        transform: translateY(-2px) rotateZ(-45deg);
-        border-bottom: 4px solid whitesmoke;
-        border-left: 4px solid whitesmoke;
-        opacity: 0;
-        transition-duration: 0.2s;
-    }
+  position: relative;
+  appearance: none;
+  margin: 0;
+  margin-right: 5px;
+  outline: none;
+  border: none;
+  &:checked::after {
+    transform: rotateZ(-45deg);
+    opacity: 1;
+  }
+  &::before {
+    content: "";
+    box-sizing: border-box;
+    transform: translate(0px, 2px);
+    display: block;
+    width: 15px;
+    height: 15px;
+    border-radius: 2px;
+    background-color: rgb(80, 80, 80);
+    transition-duration: 0.2s;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    display: block;
+    left: 1px;
+    top: 3px;
+    width: 10px;
+    height: 5px;
+    transform: translateY(-2px) rotateZ(-45deg);
+    border-bottom: 4px solid whitesmoke;
+    border-left: 4px solid whitesmoke;
+    opacity: 0;
+    transition-duration: 0.2s;
+  }
 `;
 
-type CheckboxProps = {
-    title: string,
-    value: string,
-    onChange: (value: string, checked: boolean) => any,
-    checked?: boolean
-};
+export interface ICheckboxProps {
+  title: string;
+  value: string;
+  onChange: (value: string, checked: boolean) => void;
+  checked?: boolean;
+}
 
-const Checkbox = (props: CheckboxProps) => {
-    return (
-        <Wrapper>
-            <Input
-                value={props.value}
-                checked={props.checked}
-                onChange={(e) => {
-                    props.onChange(e.target.value, e.target.checked);
-                }}
-            />
-            {props.title}
-        </Wrapper>
-    );
+const Checkbox: React.FC<ICheckboxProps> = (props) => {
+  return (
+    <Wrapper>
+      <Input
+        value={props.value}
+        checked={props.checked}
+        onChange={(e) => {
+          props.onChange(e.target.value, e.target.checked);
+        }}
+      />
+      {props.title}
+    </Wrapper>
+  );
 };
 
 export default Checkbox;
