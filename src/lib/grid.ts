76ac8c465436f7eac6e7522e96b62c571ab00f46
Yura Morozov <yurafrozen@gmail.com>
2023-04-15 23:31:36 +0500

    feat: `Grid`: removed `getOr`, `forEach`, `map`

--- src/lib/grid.ts
+++ src/lib/grid.ts
@@ -2,8 +2,6 @@ import { fixNumber, randInt } from './helpers';
 
 export type Coords = [number, number];
 
-type CallbackFn<T, U> = (obj: T | undefined, x: number, y: number) => U;
-
 export class Grid<T> {
   private readonly cells: (T | undefined)[][];
   constructor(
@@ -41,12 +39,6 @@ export class Grid<T> {
   get(x: number, y: number) {
     return this.cells[x]![y];
   }
-  getOr(x: number, y: number, value: T) {
-    const here = this.cells[x]![y];
-    return here !== undefined
-      ? here
-      : value;
-  }
   set(x: number, y: number, value: T | undefined) {
     this.cells[x]![y] = value;
   }
@@ -58,20 +50,6 @@ export class Grid<T> {
     const bufferB = this.get(x2, y2);
     this.set(x, y, bufferB);
     this.set(x2, y2, bufferA);
-  }
-  forEach(func: CallbackFn<T, void>) {
-    for (let x = 0; x < this.width; x++) {
-      for (let y = 0; y < this.height; y++) {
-        func(this.get(x, y), x, y);
-      }
-    }
-  }
-  map<U>(func: CallbackFn<T, U>): Grid<U> {
-    const next = new Grid<U>(this.width, this.height);
-    this.forEach((value, x, y) => {
-      next.set(x, y, func(value, x, y));
-    });
-    return next;
   }
   flat(): (T | undefined)[] {
     return Array.prototype.concat.apply([], this.cells);
