7e04db34b14263043cd3b4a2097f0bf6994bfa40
Yura Morozov <yurafrozen@gmail.com>
2023-04-16 14:13:13 +0500

    fix(WideButton): indent

--- src/ui/WideButton.tsx
+++ src/ui/WideButton.tsx
@@ -1,23 +1,23 @@
 import styled from 'styled-components';
 
 export const WideButton = styled.button`
-    display: block;
-    font-size: 100%;
-    text-align: center;
-    box-sizing: border-box;
-    width: 100%;
-    border-radius: 5px;
-    padding: 4px 10px;
-    color: whitesmoke;
-    background-color: rgb(80, 80, 80);
-    border: none;
-    outline: none;
-    line-height: 20px;
-    cursor: pointer;
-    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
-    transition-duration: 0.2s;
-    &:active {
-        box-shadow: none;
-        background-color: rgb(60, 60, 60);
-    }
+  display: block;
+  font-size: 100%;
+  text-align: center;
+  box-sizing: border-box;
+  width: 100%;
+  border-radius: 5px;
+  padding: 4px 10px;
+  color: whitesmoke;
+  background-color: rgb(80, 80, 80);
+  border: none;
+  outline: none;
+  line-height: 20px;
+  cursor: pointer;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+  transition-duration: 0.2s;
+  &:active {
+    box-shadow: none;
+    background-color: rgb(60, 60, 60);
+  }
 `;
