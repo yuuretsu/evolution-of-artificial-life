2248c5488295d0b4f8e36619ab5996a8a89f6045
Yura Morozov <yurafrozen@gmail.com>
2023-09-09 00:41:17 +0500

    vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths({ parseNative: true }), react()],
-})
+  plugins: [
+    tsconfigPaths({ parseNative: true }),
+    react(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
+});
