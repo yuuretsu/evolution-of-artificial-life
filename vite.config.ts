40669eef8a1dba5d3ca6bb02af7e49105ba4917a
Yura Morozov <yurafrozen@gmail.com>
2023-09-11 00:37:37 +0500

    eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
+// eslint-disable-next-line no-restricted-syntax
 export default defineConfig({
   plugins: [
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     tsconfigPaths({ parseNative: true }),
     react(),
-    VitePWA({ registerType: "autoUpdate" }),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
 });
