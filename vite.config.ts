f5a1f8087f3289b715d0a38828cc9d427a6c79a2
Yura Morozov <yurafrozen@gmail.com>
2023-09-11 22:17:23 +0500

    pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable camelcase */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -9,6 +11,32 @@ export default defineConfig({
     // eslint-disable-next-line @typescript-eslint/naming-convention
     tsconfigPaths({ parseNative: true }),
     react(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Evolution of Artificial Life',
+        short_name: 'Artificial Life',
+        background_color: 'black',
+        theme_color: 'black',
+        orientation: 'any',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '512x512@2x.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
 });
