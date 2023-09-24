e1e0555ee47ee83ce3320c37622f1a88efd5fb66
yuuretsu <yurafrozen@gmail.com>
2023-09-24 20:57:49 +0000

    Deploying to gh-pages from @ yuuretsu/evolution-of-artificial-life@9e180468e09e1b1bd1e21bab7706c8b9bfc4dee8 🚀

--- assets/index-cbec777a.js
+++ assets/index-cbec777a.js
@@ -491,7 +491,7 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
   bottom: 0;
   left: 0;
   right: 0;
-  height: 20px;
+  height: env(safe-area-inset-bottom);
   background-color: rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(20px);
   z-index: 9999;
