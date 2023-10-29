d6c4df557d9b4f7e662aa7d0482e75c0b469db60
Yurii Morozov <yurafrozen@gmail.com>
2023-10-29 21:00:49 +0500

    appStore refactoring

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,24 +1,25 @@
 import { VIEW_MODES } from 'lib/view-modes';
 import { makeAutoObservable } from 'mobx';
-import { ValueStore } from 'stores';
+import { ToggleStore, ValueStore } from 'stores';
 
 class AppStore {
   // Defaulting to paused if in development mode
-  isPaused = import.meta.env.DEV;
-
+  isPausedStore = new ToggleStore(import.meta.env.DEV === true);
   timeBetweenSteps = new ValueStore(0);
-
   viewModeName = new ValueStore<string>(Object.keys(VIEW_MODES)[0] || '');
-
   imageOffset = new ValueStore({ x: 0, y: 0 });
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  play = () => this.isPaused = false;
-  pause = () => this.isPaused = true;
-  toggleIsPaused = () => this.isPaused = !this.isPaused;
+  get isPaused() {
+    return this.isPausedStore.state;
+  }
+
+  play = this.isPausedStore.setFalse;
+  pause = this.isPausedStore.setTrue;
+  toggleIsPaused = this.isPausedStore.toggle;
 }
 
 export const appStore = new AppStore();
