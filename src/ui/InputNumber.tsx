bdf1ea4cb3d546f7d3761835271231b523bf2ba2
Yurii Morozov <yurafrozen@gmail.com>
2023-10-30 20:49:04 +0500

    InputNumber border

--- src/ui/InputNumber.tsx
+++ src/ui/InputNumber.tsx
@@ -12,11 +12,12 @@ export const InputNumber = styled.input.attrs({
   border-radius: 5px;
   padding: 3px 8px;
   line-height: 20px;
-  background-color: rgb(40, 40, 40);
+  background-color: rgb(40, 40, 40, 0.5);
   color: whitesmoke;
   transition-duration: 0.2s;
   appearance: textfield;
   -moz-appearance: textfield;
+  box-shadow: 0 0 0 1px rgba(80, 80, 80, 0.25);
   &::-webkit-inner-spin-button {
     appearance: none;
   }
