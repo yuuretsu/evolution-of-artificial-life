6f36166c430367c03ae3f98391e452e7226bc7c8
Yura Morozov <yuryy.morozov@apri-code.ru>
2024-01-13 17:51:27 +0500

    genome variables names

--- src/lib/genome/genes.ts
+++ src/lib/genome/genes.ts
@@ -38,13 +38,13 @@ export const GENES_ARR = [
     color: new Rgba(255, 255, 200),
     colorInfluence: 0.01,
     action: ({ bot, x, y, world, property }) => {
-      const F_COORDS = world.narrowToCoords(x, y, bot.narrow, 1);
-      const F_BLOCK = world.get(...F_COORDS);
+      const frontCoords = world.narrowToCoords(x, y, bot.narrow, 1);
+      const frontBlock = world.get(...frontCoords);
       bot.energy *= 0.9;
-      if (F_BLOCK) return { isCompleted: true, msg: 'Размножение не удалось: спереди блок' };
+      if (frontBlock) return { isCompleted: true, msg: 'Размножение не удалось: спереди блок' };
       if (bot.energy <= 5) return { isCompleted: true, msg: 'Размножение не удалось: мало энергии' };
       if (bot.age <= 10) return { isCompleted: true, msg: 'Размножение не удалось: бот слишком молод' };
-      world.set(...F_COORDS, bot.multiply(world.genePool, property.option));
+      world.set(...frontCoords, bot.multiply(world.genePool, property.option));
       return { isCompleted: true, msg: 'Размножение' };
     },
     translation: {
@@ -90,12 +90,12 @@ export const GENES_ARR = [
     colorInfluence: 0.01,
     action: ({ bot, x, y, world, property }) => {
       bot.energy -= 0.5;
-      const F_BLOCK = world.get(
+      const frontBlock = world.get(
         ...world.narrowToCoords(x, y, bot.narrow, 1)
       );
-      if (!F_BLOCK) return { isCompleted: true, msg: 'Атака не удалась' };
+      if (!frontBlock) return { isCompleted: true, msg: 'Атака не удалась' };
       const value = lerp(0, 5, property.option) * bot.hunterFactor ** 2;
-      const result = F_BLOCK.onAttack(value);
+      const result = frontBlock.onAttack(value);
       bot.energy += result;
       bot.increaseHunterFactor(0.01);
       bot.health = Math.min(1, bot.health + 0.01);
@@ -115,12 +115,12 @@ export const GENES_ARR = [
     action: ({ bot, x, y, world }) => {
       bot.health -= 0.99;
       bot.energy -= 0.1;
-      const F_BLOCK = world.get(
+      const frontBlock = world.get(
         ...world.narrowToCoords(x, y, bot.narrow, 1)
       );
-      if (!F_BLOCK) return { isCompleted: true, msg: 'Заражение не удалось' };
+      if (!frontBlock) return { isCompleted: true, msg: 'Заражение не удалось' };
       const genome = bot.genome.replication(world.genePool);
-      F_BLOCK.onVirus(genome, bot.familyColor.mutateRgb(5));
+      frontBlock.onVirus(genome, bot.familyColor.mutateRgb(5));
       return { isCompleted: true, msg: 'Заражение другого бота' };
     }
   } as const,
@@ -131,10 +131,10 @@ export const GENES_ARR = [
     color: new Rgba(200, 200, 200),
     action: ({ bot, x, y, world }) => {
       bot.energy -= 0.5;
-      const F_COORDS = world.narrowToCoords(x, y, bot.narrow, 1);
-      const F_BLOCK = world.get(...F_COORDS);
-      if (F_BLOCK) return { isCompleted: true, msg: 'Передвижение не удалось' };
-      world.swap(x, y, ...F_COORDS);
+      const frontCoords = world.narrowToCoords(x, y, bot.narrow, 1);
+      const frontBlock = world.get(...frontCoords);
+      if (frontBlock) return { isCompleted: true, msg: 'Передвижение не удалось' };
+      world.swap(x, y, ...frontCoords);
       return { isCompleted: true, msg: 'Передвижение' };
     }
   } as const,
@@ -146,12 +146,12 @@ export const GENES_ARR = [
     colorInfluence: 0.01,
     action: ({ bot, x, y, world, property }) => {
       bot.energy -= 0.5 * property.option;
-      const F_COORDS = world.narrowToCoords(x, y, bot.narrow, 1);
-      const F_BLOCK = world.get(...F_COORDS);
-      const O_COORDS = world.narrowToCoords(x, y, bot.narrow, -1);
-      const O_BLOCK = world.get(...O_COORDS);
-      if (!F_BLOCK || O_BLOCK) return { isCompleted: true, msg: 'Не удалось толкнуть другой объект' };
-      world.swap(...F_COORDS, ...O_COORDS);
+      const frontCoords = world.narrowToCoords(x, y, bot.narrow, 1);
+      const frontBlock = world.get(...frontCoords);
+      const otherCoords = world.narrowToCoords(x, y, bot.narrow, -1);
+      const otherBlock = world.get(...otherCoords);
+      if (!frontBlock || otherBlock) return { isCompleted: true, msg: 'Не удалось толкнуть другой объект' };
+      world.swap(...frontCoords, ...otherCoords);
       return { isCompleted: true, msg: 'Толкнул другой объект' };
     }
   } as const,
@@ -163,8 +163,8 @@ export const GENES_ARR = [
     color: new Rgba(255, 255, 255),
     action: ({ bot, x, y, world }) => {
       bot.energy -= 1;
-      const F_COORDS = world.narrowToCoords(x, y, bot.narrow, 1);
-      world.swap(...F_COORDS, x, y);
+      const frontCoords = world.narrowToCoords(x, y, bot.narrow, 1);
+      world.swap(...frontCoords, x, y);
       return { isCompleted: true, msg: 'Поменялся местами с другой клеткой' };
     }
   } as const,
@@ -224,9 +224,9 @@ export const GENES_ARR = [
     name: 'Спереди блок?',
     description: 'Следующая команда генома будет той, на которую указывает ветка 1 текущего гена, если перед ботом есть пустая клетка.Иначе следующая команда берется из ветки 2.',
     action: ({ bot, x, y, world, property }) => {
-      const F_COORDS = world.narrowToCoords(x, y, bot.narrow, 1);
-      const F_BLOCK = world.get(...F_COORDS);
-      if (F_BLOCK) {
+      const frontCoords = world.narrowToCoords(x, y, bot.narrow, 1);
+      const frontBlock = world.get(...frontCoords);
+      if (frontBlock) {
         const goto = property.branches[0];
         const msg = `Спереди блок → ${goto}`;
         return { goto, msg };
@@ -244,10 +244,10 @@ export const GENES_ARR = [
     name: 'Спереди родственник?',
     description: 'Следующая команда генома будет той, на которую указывает ветка 1 текущего гена, если перед ботом находится родственник. Иначе следующая команда берется из ветки 2.',
     action: ({ bot, x, y, world, property }) => {
-      const F_COORDS = world.narrowToCoords(x, y, bot.narrow, 1);
-      const F_BLOCK = world.get(...F_COORDS);
-      if (F_BLOCK) {
-        const familyColor = F_BLOCK.getFamilyColor();
+      const frontCoords = world.narrowToCoords(x, y, bot.narrow, 1);
+      const frontBlock = world.get(...frontCoords);
+      if (frontBlock) {
+        const familyColor = frontBlock.getFamilyColor();
         if (
           familyColor !== null &&
           familyColor.difference(bot.familyColor) > property.option
